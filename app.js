5b3a1bd0ae5206c1330f1ed906a7ec1cefdbfb26
Amanda Cambuim <amandasouzzacambuim@gmail.com>
2025-02-05 18:26:01 -0300

    Alterração do número na tela inicial

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemIncial() {
    exibirTextoNaTela('h1', 'Jogo do número secreto');
-   exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+   exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemIncial();
